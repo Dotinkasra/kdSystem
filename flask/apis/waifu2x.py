cf24dbd98332b079a5321da8b17074fb79c15a84
Dotinkasra <tyekepon@pm.me>
2022-09-30 03:58:31 +0900

    add : waifu2x APIの追加

--- flask/apis/waifu2x.py
+++ flask/apis/waifu2x.py
@@ -1,14 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for, after_this_request, send_file
-from modules.process_waifu2x import Waifu2x
-import os
+from flask import Blueprint, render_template, request, redirect, url_for, after_this_request, send_file, abort, jsonify, make_response
+from modules.module import BasicModules
+from modules.cmd_process import Waifu2x
+import os, mimetypes, gc
 
 waifu2x = Blueprint('waifu2x', __name__, url_prefix='/waifu2x')
 
 @waifu2x.route('/', methods = ['GET', 'POST'])
 def waifu2x_route():
-    upload_folder = './static/waifu2x'
-    waifu2x = Waifu2x()
-
     if not request.method == 'POST':
         return render_template('waifu2x.html')
 
@@ -16,13 +14,17 @@ def waifu2x_route():
         return 'ファイルがありません'
     
     file = request.files['file']
-    if file.filename == '':
+    if file.filename == '' or not file:
         return 'ファイルがありません'
 
-    if file:
-        file.save(os.path.join(upload_folder, file.filename))
-        result = waifu2x.do(file.filename)
-        return redirect(url_for('waifu2x_result', filename = result))
+    upload_folder = './static/waifu2x'
+    file.save(os.path.join(upload_folder, file.filename))
+
+    waifu2x = Waifu2x()
+    result = waifu2x.do(file.filename)
+    BasicModules.collect_after_deleting(waifu2x)
+
+    return redirect(url_for('waifu2x_result', filename = result))
 
 @waifu2x.route('/<string:filename>', methods = ['GET', 'POST'])
 def waifu2x_result(filename):
@@ -35,3 +37,30 @@ def waifu2x_result(filename):
             waifu2x.logger.error("Error")
         return response
     return send_file(file_path)
+
+
+@waifu2x.route('/api/post', methods = ['POST'])
+def waifu2x_api():
+    if not (request.method == 'POST' and 'file' in request.files):
+        return abort(400, 'Parameters: word \n Message: No keywords.')
+
+    file = request.files['file']
+    if file.filename == '' or not file:
+        return abort(400, 'Parameters: word \n Message: No keywords.')
+
+    upload_folder = './static/waifu2x/'
+    file.save(os.path.join(upload_folder, file.filename))
+
+    waifu2x = Waifu2x()
+    result = waifu2x.do(file.filename)
+    BasicModules.collect_after_deleting(waifu2x)
+
+    upscale_file_path = upload_folder + result
+    response = make_response()
+    response.data = open(upscale_file_path, "rb").read()
+    response.headers['Content-Disposition'] = 'attachment; filename=' + result
+    response.mimetype = mimetypes.guess_type(result)[0]
+
+    os.remove(upscale_file_path)
+    
+    return response
