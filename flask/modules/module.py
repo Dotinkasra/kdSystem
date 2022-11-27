7cdce1acc402ef59d67b0c67f18546042b07fc22
Dotinkasra <tyekepon@pm.me>
2022-11-27 17:01:46 +0900

    add : DBアクセスに関するメソッドを切り出し

--- flask/modules/module.py
+++ flask/modules/module.py
@@ -1,4 +1,5 @@
 import gc
+import sqlite3
 
 class BasicModules():
 
@@ -10,3 +11,48 @@ class BasicModules():
     def collect_after_deleting(self, v: any):
         del v
         gc.collect()
+
+class DBaccess():
+    def __init__(self, dbname) -> None:
+        self.dbname = dbname
+
+    def close(self):
+        self.conn.close()
+
+    def connect(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cur = self.conn.cursor()
+
+    def select(self, sql: str, parameta: tuple = None) -> list:
+        self.connect()
+        if parameta:
+            self.cur.execute(sql, parameta)
+        else:
+            self.cur.execute(sql)
+        result = self.cur.fetchall()
+        self.close()
+        return result
+
+    def insert(self, sql: str, parameta: tuple = None, table_name: str = None) -> None|list:
+        self.connect()
+        if parameta:
+            self.cur.execute(sql, parameta)
+        else:
+            self.cur.execute(sql)
+        self.conn.commit()
+
+        result = None
+        if table_name:
+            self.cur.execute(f"SELECT * FROM {table_name} WHERE rowid = last_insert_rowid();")
+            result = self.cur.fetchall()
+        self.close()
+        return result
+
+    def delete(self, sql: str, parameta: tuple = None):
+        self.connect()
+        if parameta:
+            self.cur.execute(sql, parameta)
+        else:
+            self.cur.execute(sql)
+        self.conn.commit()
+        self.close()
